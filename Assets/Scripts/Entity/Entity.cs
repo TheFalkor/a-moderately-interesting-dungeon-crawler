003b1e39b4c3c02a3dc314acac3d3b6bd5668ca4
Falkrik <tobaksduo@gmail.com>
2022-04-11 17:47:45 +0200

    Basic movement with WASD

--- Assets/Scripts/Entity/Entity.cs
+++ Assets/Scripts/Entity/Entity.cs
@@ -10,24 +10,36 @@ public class Entity : Occupant
         
     }
 
+    public void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.W))
+            Move(Direction.NORTH);
+        if (Input.GetKeyUp(KeyCode.A))
+            Move(Direction.WEST);
+        if (Input.GetKeyUp(KeyCode.S))
+            Move(Direction.SOUTH);
+        if (Input.GetKeyUp(KeyCode.D))
+            Move(Direction.EAST);
+    }
+
     public void Move(Direction direction)
     {
         switch (direction)
         {
             case Direction.NORTH:
-                //
+                transform.position += new Vector3(0, 1);
                 break;
 
             case Direction.EAST:
-                //
+                transform.position += new Vector3(1, 0);
                 break;
 
             case Direction.SOUTH:
-                //
+                transform.position += new Vector3(0, -1);
                 break;
 
             case Direction.WEST:
-                //
+                transform.position += new Vector3(-1, 0);
                 break;
         }
     }
